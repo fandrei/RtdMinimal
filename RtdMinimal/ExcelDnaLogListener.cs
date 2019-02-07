ca589e8ea76168b8803834fedbeef00ae49b8bf2
Andrei Faber <andrei.faber@gmail.com>
2019-02-07 15:51:40 -0600

    custom trace listener

---
+++ RtdMinimal/ExcelDnaLogListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace RtdMinimal
+{
+    public class ExcelDnaLogListener : TraceListener
+    {
+        public ExcelDnaLogListener()
+        {
+            Trace.WriteLine($"--------------- {nameof(ExcelDnaLogListener)}");
+        }
+
+        public ExcelDnaLogListener(string name)
+            : base(name)
+        {
+            Trace.WriteLine($"--------------- {nameof(ExcelDnaLogListener)}");
+        }
+
+        public override void Write(string message)
+        {
+            Trace.WriteLine($"### {message}");
+        }
+
+        public override void WriteLine(string message)
+        {
+            Trace.WriteLine($"### {message}");
+        }
+    }
+}
