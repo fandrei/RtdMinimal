fe50fa2222adf8c97d1ab1c4fbe53cfe6d386bb0
Andrei Faber <andrei.faber@gmail.com>
2019-03-26 16:36:58 -0600

    file log

--- RtdMinimal/ExcelDnaLogListener.cs
+++ RtdMinimal/ExcelDnaLogListener.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -11,22 +12,46 @@ namespace RtdMinimal
         public ExcelDnaLogListener()
         {
             Trace.WriteLine($"--------------- {nameof(ExcelDnaLogListener)}");
+            InitFile();
         }
 
         public ExcelDnaLogListener(string name)
             : base(name)
         {
             Trace.WriteLine($"--------------- {nameof(ExcelDnaLogListener)}");
+            InitFile();
+        }
+
+        void InitFile()
+        {
+            lock (Sync)
+            {
+                if (_writer == null)
+                    _writer = new StreamWriter($"Log_{DateTime.Now:yyyy-MM-dd HH_mm_ss.fffffff}.txt");
+            }
         }
 
         public override void Write(string message)
         {
-            Trace.WriteLine($"{Name} {message}");
+            Trace.Write($"{Name} {message}");
+            lock (Sync)
+            {
+                _writer.Write($"{Name} {message}");
+                _writer.Flush();
+            }
         }
 
         public override void WriteLine(string message)
         {
             Trace.WriteLine($"{Name} {message}");
+            lock (Sync)
+            {
+                _writer.WriteLine($"{Name} {message}");
+                _writer.Flush();
+            }
         }
+
+        private static StreamWriter _writer;
+        static readonly object Sync = new object();
     }
 }
